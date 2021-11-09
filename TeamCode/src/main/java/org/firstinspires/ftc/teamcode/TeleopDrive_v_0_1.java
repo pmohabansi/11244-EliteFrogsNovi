ec2c2e2e5e39ee075ce57a557f078f998ab8c322
Piyush Mohabansi <piyush.mohabansi@gmail.com>
2021-11-08 19:54:53 -0500

    Update TeleopDrive_v_0_1.java
    added the code to move DCMotor to inches only.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopDrive_v_0_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopDrive_v_0_1.java
@@ -31,7 +31,7 @@ public class TeleopDrive_v_0_1 extends LinearOpMode {
     // Define variables for motors which are connected` to the wheels to rotate.
     DcMotor leftFrontWheelMotor = null;
     DcMotor rightFrontWheelMotor = null;
-    DcMotor leftRearWheelMotor   = null;
+    DcMotor leftRearWheelMotor = null;
     DcMotor rightRearWheelMotor = null;
     DcMotor carouselWheelMotor = null;
     DcMotor armWheelMotor = null;
@@ -40,6 +40,14 @@ public class TeleopDrive_v_0_1 extends LinearOpMode {
 
     // Declare LinearOpMode members.
     ElapsedTime runtime = new ElapsedTime();
+
+    static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 0.5;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 20.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+                                          (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double TURN_SPEED = 0.5;
+
 
     @Override
     public void runOpMode() {
@@ -73,10 +81,9 @@ public class TeleopDrive_v_0_1 extends LinearOpMode {
         leftFrontWheelMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightFrontWheelMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         carouselWheelMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        armWheelMotor.setTargetPosition(armWheelMotor.getCurrentPosition());
+
         armWheelMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armWheelMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        armWheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armWheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -181,22 +188,13 @@ public class TeleopDrive_v_0_1 extends LinearOpMode {
                 // This is to lift the arm Up
                 telemetry.addLine("arm lift Up");
 
-                armWheelPower = Range.clip(gamepad2.right_trigger, -wheelPowerLimit, wheelPowerLimit);
-                armNewPosition = armNewPosition + 1;
-                armWheelMotor.setTargetPosition(armNewPosition);
-                armWheelMotor.setPower(armWheelPower);
-            } else if(gamepad2.left_trigger != 0) {
+                encoderDrive(TURN_SPEED, 5, 4.0);
+            } else if (gamepad2.left_trigger != 0) {
                 // This is to lift the arm Down
                 telemetry.addLine("arm lift Down");
 
-                armWheelPower = Range.clip(-gamepad2.left_trigger, -wheelPowerLimit, wheelPowerLimit);
-                armNewPosition = armNewPosition * -1;
-                armWheelMotor.setTargetPosition(armNewPosition);
-                armWheelMotor.setPower(armWheelPower);
-                while (armWheelMotor.isBusy()) {
-                    sleep(10);
-                }
-            }else if (gamepad2.right_bumper) {
+                encoderDrive(TURN_SPEED, -5, 4.0);
+            } else if (gamepad2.right_bumper) {
                 telemetry.addLine("Open the Claw");
                 servoLeftClawPosition = 0;
                 servoRightClawPosition = 0;
@@ -216,8 +214,6 @@ public class TeleopDrive_v_0_1 extends LinearOpMode {
             leftClawMotor.setPosition(servoLeftClawPosition);
             rightClawMotor.setPosition(servoRightClawPosition);
 
-            armWheelMotor.setPower(armWheelPower);
-
             telemetry.addData("Motors", "front left (%.2f), front right (%.2f), rear left (%.2f)" +
                             ", rear right (%.2f), carosel (%.2f), arm (%.2f), newPosition (%d), arm position (%d)," +
                             " LeftClaw (%.2f), RightClaw (%.2f).",
@@ -227,5 +223,53 @@ public class TeleopDrive_v_0_1 extends LinearOpMode {
 
             telemetry.update();
         }
+    }
+
+    /*
+     *  Method to perfmorm a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the opmode running.
+     */
+    public void encoderDrive(double speed,
+                             double armShiftInInches,
+                             double timeoutS) {
+        int newArmTarget;
+
+
+        // Determine new target position, and pass to motor controller
+        newArmTarget = armWheelMotor.getCurrentPosition() + (int) (armShiftInInches * COUNTS_PER_INCH);
+        armWheelMotor.setTargetPosition(newArmTarget);
+
+        // Turn On RUN_TO_POSITION
+        armWheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion.
+        runtime.reset();
+        armWheelMotor.setPower(Math.abs(speed));
+
+        // keep looping while we are still active, and there is time left, and both motors are running.
+        // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+        // its target position, the motion will stop.  This is "safer" in the event that the robot will
+        // always end the motion as soon as possible.
+        // However, if you require that BOTH motors have finished their moves before the robot continues
+        // onto the next step, use (isBusy() || isBusy()) in the loop test.
+        while (opModeIsActive() &&
+                (runtime.seconds() < timeoutS) &&
+                armWheelMotor.isBusy()) {
+
+            // Display it for the driver.
+            telemetry.addData("Path1", "Running to %7d", newArmTarget);
+            telemetry.addData("Path2", "Running at %7d", armWheelMotor.getCurrentPosition());
+            telemetry.update();
+        }
+
+        // Stop all motion;
+        armWheelMotor.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        armWheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
